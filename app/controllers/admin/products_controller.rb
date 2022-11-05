9aa8525d98cb3c3c9f6fdf999e11c3de103f1ad2
trevorfunk <trevorjonfunk@gmail.com>
2022-11-05 16:01:25 -0700

    add admin dashboard feature

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["AUTH_USERNAME"].to_s, password: ENV["AUTH_PASSWORD"].to_s
+  http_basic_authenticate_with name: ENV["USERNAME"].to_s, password: ENV["PASSWORD"].to_s
 
   def index
     @products = Product.order(id: :desc).all
