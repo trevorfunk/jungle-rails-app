1da6d043475b32aa7a5099f92657c98a5dab02a8
trevorfunk <trevorjonfunk@gmail.com>
2022-11-13 17:18:47 -0800

    add user-spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,26 @@ class User < ApplicationRecord
 
  has_secure_password
 
+ validates :first_name, presence: true
+ validates :last_name, presence: true
+ validates :email, presence: true, uniqueness: { case_sensitive: false }
+ validates :password, presence: true, length: {minimum: 7}, confirmation: true
+ validates :password_confirmation, presence: true, length: {minimum: 7}
+
+ def self.authenticate_with_credentials(email, password)
+   email.downcase!
+   email.strip!
+   @user = User.find_by_email(email)
+   @user ? user.authenticate(password) ? user : nil : nil
+ end
+
+ before_save :email_correction
+
+ private
+
+ def email_check
+   self.email.downcase!
+   self.email.strip!
+ end
+
 end
